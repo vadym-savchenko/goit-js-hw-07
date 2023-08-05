19d7287ad159ddd73f70494f8d61ee24736476ed
vadym-savchenko <wadim.zp@gmail.com>
2023-08-05 17:12:02 +0300

    add 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const markupGallery = galleryItems.map(({ preview, original, description }) => `
+<li class="gallary__item">
+<a class="gallary__link" href="${original}">
+    <img 
+    class="gallery__image"
+    src="${preview}" 
+    alt="${description}"/>
+ </a>
+ </li>`).join("");
+
+
+galleryEl.insertAdjacentHTML("afterbegin", markupGallery);
+ 
+
+
+const galleryUsage = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+}); 
+
+
+
+
+
+
+
+
+
+
+// console.log(galleryItems);
